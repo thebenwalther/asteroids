630fcc7a22d3944312f4fc5a94213964f244c8a7
Ben Walther <thebenwalther@gmail.com>
2026-05-22 15:41:41 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+def main():
+    print("Starting Asteroids with pygame version: 2.6.1")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
