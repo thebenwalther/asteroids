b6b81571b936a31ccc96595cf492ca0a27da9c93
Ben Walther <thebenwalther@gmail.com>
2026-05-28 11:55:27 -0500

    astroid game

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import sys
 from logging import log
+from pydoc import plainpager
+from turtle import up
 
 import pygame
 
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from constants import (
+    ASTEROID_KINDS,
+    ASTEROID_MAX_RADIUS,
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_SPAWN_RATE_SECONDS,
+    PLAYER_RADIUS,
+    PLAYER_SPEED,
+    PLAYER_TURN_SPEED,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+)
+from logger import log_event, log_state
+from player import Player
+from shot import Shot
 
 
 def main():
@@ -11,13 +28,40 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroidfield = AsteroidField()
+    Shot.containers = (shots, updatable, drawable)
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        for shot in list(shots):
+            for asteroid in list(asteroids):
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
+                    break
         screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
 
